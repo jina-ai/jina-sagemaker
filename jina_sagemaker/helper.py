c0255e6ceb8afff829e0eebf8bc1bd06885d17dc
Zac Li <zac.li@jina.ai>
2023-10-12 16:37:36 +0800

    fix: black

--- jina_sagemaker/helper.py
+++ jina_sagemaker/helper.py
@@ -6,13 +6,12 @@ import uuid
 def prefix_csv_with_ids(input_path, output_path):
     """Prefix each line in the CSV with a random ID."""
 
-    with open(input_path, 'r') as csv_file, tempfile.NamedTemporaryFile(
-        mode='w+', delete=False
+    with open(input_path, "r") as csv_file, tempfile.NamedTemporaryFile(
+        mode="w+", delete=False
     ) as temp_file:
-
         for line in csv_file:
             text = line.strip()
-            uid = str(uuid.uuid4()).replace('-', '')[:32]
+            uid = str(uuid.uuid4()).replace("-", "")[:32]
             temp_file.write(f"{uid},{text}\n")
 
     # Replace the original file with the temporary one
