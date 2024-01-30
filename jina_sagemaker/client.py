94f1831dc01d7593e12518c6d62faf267785b18c
Delgermurun Purevkhuu <delgermurun@users.noreply.github.com>
2024-01-30 18:24:29 +0100

    fix: pass boto session on sagemaker session

--- jina_sagemaker/client.py
+++ jina_sagemaker/client.py
@@ -28,7 +28,10 @@ class Client:
 
         self._sm_runtime_client = boto3.client("sagemaker-runtime", **client_args)
         self._sm_client = boto3.client("sagemaker", **client_args)
-        self._sm_session = sagemaker.Session(sagemaker_client=self._sm_client)
+        self._sm_session = sagemaker.Session(
+            boto_session=boto3.Session(**client_args),
+            sagemaker_client=self._sm_client,
+        )
         self._aas_client = boto3.client("application-autoscaling", **client_args)
         self._cw_client = boto3.client("cloudwatch", **client_args)
 
